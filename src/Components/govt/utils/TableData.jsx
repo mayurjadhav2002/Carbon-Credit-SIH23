4dd6d0ab2fc1c4818b6da2894ef909e238851e67
FIRST_NAME LAST_NAME <dsmanishsingh@gmail.com>
2023-09-26 12:56:44 +0530

    Manihs

--- src/Components/govt/utils/TableData.jsx
+++ src/Components/govt/utils/TableData.jsx
@@ -72,7 +72,7 @@ function TableData() {
                     </thead>
                     <tbody>
                        
-                        {data.map((data, index)=>
+                        {currentItems.map((data, index)=>
                             <tr key={data.id} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 text-center">
                             <th key="index" scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {data.name}
